a717aa05cbb928d1be08c3904e4e4e83c19760c9
CMD SHAKEEL <cmdshakeel21@gmail.com>
2021-07-13 21:42:33 +0530

    Added:- Snake ladder UC1 is added

---
+++ SnakeAndLadderUC1.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+
+public class SnakeAndLadderUC1 {
+	public static  int source = 0 ;
+	public static  int destination = 100 ;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("WELCOME TO SNAKE AND LADDER");
+		System.out.println("--------**********----------");
+	
+		int player1 =source;
+		int player2 =destination;
+		
+		System.out.println("player start at this point 0");
+		System.out.println("player starting point"+ player1);
+		System.out.println();
+		System.out.println("player end at this point 100");
+		System.out.println("player WINNIG point"+ player2);
+		  
+	}
+
+}
