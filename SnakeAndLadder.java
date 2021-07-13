262f26a27b1e67ecc378c4294faedd5b971f8158
CMD SHAKEEL <cmdshakeel21@gmail.com>
2021-07-13 21:05:30 +0530

    WELCOME_TO_SNAKE_AND_LADDER_PROGRAMS

---
+++ SnakeAndLadder.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class SnakeAndLadder {
+	public static void main(String[] args) {
+		
+		
+		System.out.println("---------------WELCOME_TO_SnakeAndLadder-------------");
+
+	}
+
+}
